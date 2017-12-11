f8ee720d661589dae8a74f5ceb1e61a662b43e83
sunq <skjukjnqqqq@gmail.com>
2017-12-11 20:43:16 +0900

    Update

--- demo.py
+++ demo.py
@@ -3,21 +3,21 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import sys,random
-
+import time
 args = sys.argv
 
 G = nx.DiGraph()
 # make grahp
 print args
 if len(args) == 1:
-    G.add_node(0, data=23)
-    G.add_node(1, data=8)
-    G.add_node(2, data=2)
-    G.add_node(3, data=10)
-    G.add_node(4, data=0)
-    G.add_node(5, data=67)
-    G.add_node(6, data=22)
-    G.add_node(7, data=43)
+    G.add_node(0, data=[23])
+    G.add_node(1, data=[8])
+    G.add_node(2, data=[2])
+    G.add_node(3, data=[10])
+    G.add_node(4, data=[67])
+    G.add_node(5, data=[67])
+    G.add_node(6, data=[22])
+    G.add_node(7, data=[8])
 elif args[1] == "-r":
     G.add_node(0, data=random.randint(1,100))
     G.add_node(1, data=random.randint(1,100))
@@ -30,13 +30,13 @@ elif args[1] == "-r":
 else:
     print "invalid Options"
 
-G.add_node(8, data=[])
-G.add_node(9, data=[])
-G.add_node(10, data=[])
-G.add_node(11, data=[])
-G.add_node(12, data=[])
-G.add_node(13, data=[])
-G.add_node(14, data=[])
+G.add_node(8, data=[{},{0:"R",1:"L"}])
+G.add_node(9, data=[{},{2:"R",3:"L"}])
+G.add_node(10, data=[{},{4:"R",5:"L"}])
+G.add_node(11, data=[{},{6:"R",7:"L"}])
+G.add_node(12, data=[{},{8:"R",9:"L"}])
+G.add_node(13, data=[{},{10:"R",11:"L"}])
+G.add_node(14, data=[{},{12:"R",13:"L"}])
 
 G.add_edges_from([(0, 8),
                   (1, 8),
@@ -77,7 +77,7 @@ pos = { 0 : (0, 80),
         } 
 
 def getdata(name):
-    return str(G.nodes(data=True)[name][1]['data'])
+    return G.nodes(data=True)[name][1]['data']
 
 def display():
     #reset graph
@@ -104,31 +104,74 @@ def display():
     plt.pause(4)
 
 def support_server(a,b):
+    #サポートサーバーが上に送信するときの処理
     for support in range(a,b):
-        X = G.nodes(data=True)[support][1]['data'][0]
-        Y = G.nodes(data=True)[support][1]['data'][1]
+        support_server_data = G.nodes(data=True)[support][1]['data'][0]
+        child_node_table = G.nodes(data=True)[support][1]['data'][1]
+        R_name = child_node_table.keys()[child_node_table.values().index("R")]
+        L_name = child_node_table.keys()[child_node_table.values().index("L")]
+        #仮補整データ0の埋め込み
+        #ノード番号から
+        flags = []
+        for node_name in support_server_data.keys():
+            flags.append(child_node_table[node_name])
+
+        if "R" not in flags:
+            print child_node_table.keys()[child_node_table.values().index("R")]
+            support_server_data[child_node_table.keys()[child_node_table.values().index("R")]] = 0
+            support_server_data["Flag"] = str(R_name) + "R"
+            display()
+        elif "L" not in flags:
+            support_server_data[child_node_table.keys()[child_node_table.values().index("L")]] = 0
+            support_server_data["Flag"] = str(L_name) + "L"
+            display()
+
+        X = support_server_data[R_name]
+        Y = support_server_data[L_name]
         print "[LOG] Sending data at server" + str(support) + " " + str(X) + "+" + str(Y)
         send_data = int(X) + int(Y)
         save_data = int(X) - int(Y)
         #save
-        del G.nodes(data=True)[support][1]['data'][0:]
-        G.nodes(data=True)[support][1]['data'].append(save_data)
+        print G.nodes(data=True)[support][1]['data'][0]
+        if "Flag" in support_server_data.keys():
+                print "Flag!!!!!!!!!!"
+                save = support_server_data["Flag"][-1]
+                sendRL = support_server_data["Flag"][:-1]
+                print save
+                print sendRL
+                G.nodes(data=True)[support][1]['data'][0] = {}
+                G.nodes(data=True)[support][1]['data'][0]["Flag"] = save
+        else:
+                G.nodes(data=True)[support][1]['data'][0] = {}
         #send
         parentis = G.succ[support].keys()[0]
-        G.nodes(data=True)[parentis][1]['data'].append(send_data)
+        next_support_server = G.nodes(data=True)[parentis][1]['data'][0]
+        next_support_server[support] = send_data
+        
 
-def sending_edge():
-    for i in range(8):
-        parentis = G.succ[i].keys()[0]
-        G.nodes(data=True)[parentis][1]['data'].append(getdata(i))
-
+def sending_edge(err):
+    if err < 0:
+        for i in range(8):
+            parentis = G.succ[i].keys()[0]
+            support_server_data = G.nodes(data=True)[parentis][1]['data']
+            support_server_data[0][i]  = getdata(i)[0]
+    else:
+        for i in range(8):
+            if err == i:
+                continue
+            else:
+                parentis = G.succ[i].keys()[0]
+                support_server_data = G.nodes(data=True)[parentis][1]['data']
+                support_server_data[0][i] = getdata(i)[0]
+        
 def sending_data_process():
+    #エッジサーバーからサーバーまで一連の流れを定義する
     print "[LOG] Start send Processing"
     for clock in range(6):
         if clock == 1:
             display()
         if clock == 2:
-            sending_edge()
+            sending_edge(-1)
             display()
         if clock == 3:
             support_server(8,12)
@@ -138,6 +181,7 @@ def sending_data_process():
             display()
 
 def server_restore(server):
+    #サーバーからサポートサーバーに詳細なデータを要求する処理
     server_data = G.nodes(data=True)[server][1]['data']
     children = G.pred[server].keys()
     if server_data[0] > server_data[1]:
@@ -150,6 +194,7 @@ def server_restore(server):
         print "ignore"
 
 def support_restore(a,b,edge):
+    #サポートサーバーが、下のサポートサーバーに復元要求する処理
     #a,bはその階層の範囲　edgeは、edgeserverの次のノードかどうか
     #support server [13,["R",143]] when restore from server
     for support in range(a,b):
@@ -194,6 +239,27 @@ def restore_data_process():
             support_restore(8,12,edge=True)
             display()
 
-sending_data_process()
-restore_data_process()
+def sending_data_process_error():
+    print "[LOG] Start Processing of restore with correct data."
+    for clock in range(6):
+        #command = raw_input()
+        #if command == "n":
+        #    print "GO"
+        if clock == 1:
+            display()
+        if clock == 2:
+            sending_edge(5)
+            display() 
+        if clock == 3:
+            support_server(8,12)
+            display()
+        if clock == 4:
+            support_server(12,14)
+            display()
+
+
+#sending_data_process()
+#restore_data_process()
+
+sending_data_process_error()
 
